956062b23881b6a65938d4596080642a878fe1c3
Антон Неверович <dt@dilibrium.ru>
2019-09-13 21:44:07 +0300

    replace letter algorithms

--- 90_replace_letters.py
+++ 90_replace_letters.py
@@ -1,13 +1,16 @@
+# conditions: translate text
 a = ["g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr",
      "amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q",
      "ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb.",
      "lmu ynnjw ml rfc spj."]
+
+# data preparation
 data = []
 result = []
 for i in range(len(a)):
     data += a[i].split()
 
-
+# replacing algorithm
 for i in range(len(data)):
     letters = list(data[i])
     for j in range(len(letters)):
@@ -17,14 +20,11 @@ for i in range(len(data)):
             letters[j] = 'a'
         if ord(letters[j]) == 122:
             letters[j] = 'b'
-
         result.append(letters[j])
     result.append(' ')
 
+# result output
 result = ''.join(str(e) for e in result)
-
-print ord('a')
-print ord('z')
 print result
 
 
