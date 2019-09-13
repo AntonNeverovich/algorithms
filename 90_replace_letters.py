1ec5e4665389e1b11868a084496ea05084908340
Антон Неверович <dt@dilibrium.ru>
2019-09-13 21:40:04 +0300

    add replace exit
    q

---
+++ 90_replace_letters.py
@@ -0,0 +1,30 @@
+a = ["g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr",
+     "amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q",
+     "ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb.",
+     "lmu ynnjw ml rfc spj."]
+data = []
+result = []
+for i in range(len(a)):
+    data += a[i].split()
+
+
+for i in range(len(data)):
+    letters = list(data[i])
+    for j in range(len(letters)):
+        if 97 <= ord(letters[j]) <= 120:
+            letters[j] = chr(ord(letters[j]) + 2)
+        if ord(letters[j]) == 121:
+            letters[j] = 'a'
+        if ord(letters[j]) == 122:
+            letters[j] = 'b'
+
+        result.append(letters[j])
+    result.append(' ')
+
+result = ''.join(str(e) for e in result)
+
+print ord('a')
+print ord('z')
+print result
+
+
